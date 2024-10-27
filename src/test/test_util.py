5cdbcaac9e1f3a8fd7277e7f5e555a21402fd604
Andy <bubbaandy89@gmail.com>
2024-10-27 15:22:15 -0400

    feat: Unit test of configure_structlog

--- src/test/test_util.py
+++ src/test/test_util.py
@@ -1,7 +1,11 @@
 import logging
 
-from pytacs.util import get_log_level_from_string
+from pytacs.util import configure_structlog, get_log_level_from_string
 
 
 def test_get_log_level_from_string():
     assert get_log_level_from_string("debug") == logging.DEBUG
+
+
+def test_configure_structlog():
+    configure_structlog()
